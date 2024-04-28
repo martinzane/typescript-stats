80073d6f3bcc85de7abd9c9c9a43bb3772e64774
Martin Zane <martin@zaneweb.com>
2024-04-28 14:36:15 +0200

    Add a Result enum for clarity

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ const matches = readFileSync("data/football.csv", {
   .split("\n")
   .map((row: string): string[] => row.split(","));
 
+enum Result {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
 const manUnitedWins = matches.filter(
   (match) =>
-    (match[1] === "Man United" && match[5] === "H") ||
-    (match[2] === "Man United" && match[5] === "A")
+    (match[1] === "Man United" && match[5] === Result.HomeWin) ||
+    (match[2] === "Man United" && match[5] === Result.AwayWin)
 );
 
 console.log("Man United wins: ", manUnitedWins.length);
