7c7e2caeb3ecdb2d163a79db3b7a1c8a86361708
Martin Zane <martin@zaneweb.com>
2024-04-28 16:33:52 +0200

    Add generate function to summary

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
+import WinsAnalysis from "./analyzers/wins-analysis";
 import CsvFileReader from "./csv-file-reader";
 import MatchReader from "./match-reader";
+import ConsoleReport from "./report-targets/console-report";
+import Summary from "./summary";
 
 const csvFileReader = new CsvFileReader("data/football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(matchReader.matches);
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
+
+summary.generate(matchReader.matches);
