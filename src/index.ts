5d136b0b10579939f8047f972548b23a4f65fede
Martin Zane <martin@zaneweb.com>
2024-04-28 14:18:06 +0200

    Parse & read data from .csv file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+import { readFileSync } from "fs";
+
+const matches = readFileSync("data/football.csv", {
+  encoding: "utf-8",
+})
+  .split("\n")
+  .map((row: string): string[] => row.split(","));
+
+console.log(matches);
